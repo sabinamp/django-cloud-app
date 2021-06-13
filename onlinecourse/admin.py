4b8cc1e1a8624877152b5a25eedca8887d3db72a
Sabina Maria <p_sabinamaria@yahoo.com>
2021-06-13 22:13:46 +0200

    add Question, Choice and some data using the admin site

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Choice, Question
+
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
+
+class ChoiceInline(admin.StackedInline):
+    model: Choice
+    question = ['question']
+    is_correct = ['is_correct']
+    actions = ['enable_selected', 'disable_selected']
+
+    def enable_selected(self):
+        self.is_correct = True
+
+    def disable_selected(self):
+        self.is_correct = False
+
+
+class QuestionInline(admin.StackedInline):
+    model: Question
+    lesson = ['lesson']
+    extra = 3
 
 
 class LessonInline(admin.StackedInline):
@@ -23,7 +42,8 @@ class LessonAdmin(admin.ModelAdmin):
 
 
 # <HINT> Register Question and Choice models here
-
+admin.site.register(Question)
+admin.site.register(Choice)
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
